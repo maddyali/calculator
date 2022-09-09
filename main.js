764592c64b35586b6906517ea38070b82fdbeda5
Maddy Ali <maddyoali@gmail.com>
2022-09-08 19:14:52 -0500

    Save firstOperand & print current & last operation

--- main.js
+++ main.js
@@ -15,16 +15,46 @@ const operate = function (operator, a, b) {
 };
 
 const numberBtns = document.querySelectorAll(".number");
-const screen = document.querySelector(".screen");
+const currentOperationScreen = document.querySelector(
+  "#currentOperationScreen"
+);
+const lastOperationScreen = document.querySelector("#lastOperationScreen");
+
+const operatorBtns = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
 
 numberBtns.forEach((button) => {
   button.addEventListener("click", () => displayNumber(button.textContent));
 });
 
+operatorBtns.forEach((button) => {
+  button.addEventListener("click", () => setOperation(button.textContent));
+});
+
+equals.addEventListener("click", () => console.log("equals"));
+
+let firstOperand;
+let secondOperand;
+let currentOperation = null;
+let resetScreenState = false;
+
 function displayNumber(number) {
-  if (screen.textContent === "0") {
-    screen.textContent = number;
+  if (currentOperationScreen.textContent === "0" || resetScreenState) {
+    resetScreen();
+    currentOperationScreen.textContent = number;
   } else {
-    screen.textContent += number;
+    currentOperationScreen.textContent += number;
   }
+}
+
+function setOperation(operator) {
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} `;
+  resetScreenState = true;
+}
+
+function resetScreen() {
+  currentOperationScreen.textContent = " ";
+  resetScreenState = false;
 }
